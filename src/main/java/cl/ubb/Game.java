e619bc58637d5ee9dda50abc80121c4c8f0da38b
Gustavo <gussanma@alumnos.ubiobio.cl>
2017-04-27 02:01:26 -0400

    Segundo comit, segundo test(obtener en todos los rolls puntuacion cero

--- src/main/java/cl/ubb/Game.java
+++ src/main/java/cl/ubb/Game.java
@@ -1,10 +1,8 @@
 package cl.ubb;
 
 public class Game {
-	int score;
-	public Game(){
-		score=0;
-	}
+	private int score=0;
+	
 	public int score(){
 		return score;
 	}
--- src/test/java/cl/ubb/GameTest.java
+++ src/test/java/cl/ubb/GameTest.java
@@ -17,11 +17,22 @@ public class GameTest {
 
 	@Test
 	public void rollthreeAndRollFiveShoulbeEight() { // En un solo frame obtener 2 rolls de puntuación cero
+		for(int i=0;i<1;i++) {
 		game.roll(3);
 		game.roll(5);
 		result=game.score();
+		}
 		
 		assertEquals(8,result);
 	}
+	@Test
+	public void GetInAllFramesScoreZero() { //Tener puntacion cero en todos los rolls
+		for (int i =0; i<20;i++) {
+			game.roll(0);
+			result=game.score();
+		}
+		assertEquals(0,result);
+	}
+	
 
 }
